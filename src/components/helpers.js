8746ef78347d13350632517a97218e3f7bbaab5e
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-09-19 22:05:27 +0300

    Create helpers.js

---
+++ src/components/helpers.js
@@ -0,0 +1,22 @@
+// src/components/helpers.js
+import React from 'react';
+
+export const renderWithClickableKalips = (text, kalips, onKalipClick) => {
+  if (!text) return null;
+  if (!kalips || kalips.length === 0) {
+    return <p dangerouslySetInnerHTML={{ __html: text.replace(/\n/g, '<br />') }} />;
+  }
+  const regex = new RegExp(`(${kalips.map(k => k.kalip.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join('|')})`, 'gi');
+  const parts = text.split(regex);
+  return (
+    <p>
+      {parts.map((part, index) => {
+        const matchingKalip = kalips.find(k => k.kalip.toLowerCase() === part.toLowerCase());
+        if (matchingKalip) {
+          return <span key={index} className="clickable-kalip" onClick={() => onKalipClick(matchingKalip)}>{part}</span>;
+        }
+        return <span key={index} dangerouslySetInnerHTML={{ __html: part.replace(/\n/g, '<br />') }} />;
+      })}
+    </p>
+  );
+};
