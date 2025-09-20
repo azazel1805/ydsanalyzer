2a18bb2a231f490c8e3c4cc238daa511b381f36b
Onur Tosuner <128583602+azazel1805@users.noreply.github.com>
2025-09-21 00:08:10 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,7 @@
   publish = "dist"
   functions = "netlify/functions"
 
-# Bu blok, build işlemi sırasında kullanılacak ortam değişkenlerini tanımlar.
-# SECRETS_SCAN_OMIT_KEYS değişkenini burada tanımlayarak build işlemine 
-# bu kuralı bilerek başlamasını sağlıyoruz.
-[build.environment]
-  SECRETS_SCAN_OMIT_KEYS = "VITE_FIREBASE_API_KEY,VITE_FIREBASE_APP_ID,VITE_FIREBASE_AUTH_DOMAIN,VITE_FIREBASE_MESSAGING_SENDER_ID,VITE_FIREBASE_PROJECT_ID,VITE_FIREBASE_STORAGE_BUCKET"
+
 
 [[redirects]]
   from = "/api/*"
